77e5794c2ba337f6c7a7f38ab893ad842fa3af63
tomimarkus991 <tomimarkusalber@gmail.com>
2022-09-25 13:59:21 +0300

    changes

--- src/components/wordle/Keypad.tsx
+++ src/components/wordle/Keypad.tsx
@@ -40,7 +40,7 @@ export const Keypad = ({ usedKeys, handleKeyup }: Props) => {
   };
 
   return (
-    <div className="mx-5 my-auto mt-10 flex flex-col items-center justify-center space-y-2">
+    <div className="mx-5 mt-10 flex scale-50 flex-col items-center justify-center space-y-2 md:scale-100">
       <div className="space-x-1">
         {keys[0].map(letter => {
           return createButton(letter.key);
